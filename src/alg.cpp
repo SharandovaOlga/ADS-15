fb4e9dcf7368da8b9b930f164003fe6f724538f5
SharandovaOlga <75723905+SharandovaOlga@users.noreply.github.com>
2022-06-10 17:50:00 +0300

    Sharandova ADS-9

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,27 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
+#include  <string>
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  BST<std::string> tree;
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "Error!" << std::endl;
+    throw std::string("Can not open file!");
+  }
+  std::string word = "";
+  while (!file.eof()) {
+    char chr = file.get();
+    int ascii = chr;
+    if ((ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122)) {
+      word += tolower(ascii);
+    } else {
+      if (word != "") {
+        tree.add(word);
+      }
+    }
+  }
+  file.close();
+  return tree;
 }
